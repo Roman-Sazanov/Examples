b6cd9e5d36c89337c99f20144f6d59b2b25137b9
Roman Sazanov <r.v.1990@yandex.ru>
2023-05-22 21:53:24 +0300

    Added Condition If-Else

---
+++ Example005_Condition_If-Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Username: ");
+string Username = Console.ReadLine();
+
+if(Username.ToLower() == "masha")
+{
+    Console.WriteLine("Yessss!! Masha with us now!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(Username);
+}
